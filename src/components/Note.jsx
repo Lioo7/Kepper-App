8c658702a3ef3912a60cffcadd1e28cd50b18f35
Lioz Akirav <lioz.akirav@gmail.com>
2023-08-02 04:29:48 +0300

    render the notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Note () {
+function Note (props) {
   return (
-        <div className='note'>
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
-        </div>
+    <div className='note'>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
   )
+}
+
+Note.propTypes = {
+  title: PropTypes.string.isRequired, // Validate 'title' prop as a required string
+  content: PropTypes.string.isRequired // Validate 'content' prop as a required string
 }
 
 export default Note
